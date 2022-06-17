249e59deff98725560c5da8b0c4b0e5c5df3e481
José Marcio Britto <josemarcio_sb@hotmail.com>
2022-06-16 21:46:09 -0300

    resources and functions chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,37 @@
 import './bootstrap';
 
-import { createApp, h } from 'vue';
+import { Vue, createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import moment from 'moment';
+
+
+moment.locale('pt-br');
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => require(`./Pages/${name}.vue`),
     setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
+        const myapp = createApp({ render: () => h(app, props) })
             .use(plugin)
-            .mixin({ methods: { route } })
-            .mount(el);
+            .mixin({ methods: { route } });
+
+            myapp.config.globalProperties.$filters = {
+                formatDate(value) {
+                    if (value) {
+                        return moment(value).format('DD/MM/YYYY HH:mm');
+                    }
+                },
+            }
+            myapp.mount(el);
+            return myapp;
     },
 });
+
+
 
 InertiaProgress.init({ color: '#4B5563' });
